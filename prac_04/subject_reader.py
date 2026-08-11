d525fef73c30b9d55503e942fe0ccfa6807eef3c
jd119636 <indra.kiiver@my.jcu.edu.au>
2026-08-11 15:01:56 +1000

    adding display_subjects and refavtoring load_subjects.py

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -5,27 +5,35 @@ Data file -> lists program
 
 FILENAME = "subject_data.txt"
 
-
 def main():
     """Program to load and display subject data from file."""
-    data = load_data(FILENAME)
-    print(data)
+    subjects = load_subjects(FILENAME)
+    display_subjects(subjects)
 
 
-def load_data(filename=FILENAME):
+def load_subjects(filename=FILENAME):
     """Read data from file formatted like: subject,lecturer,number of students."""
+    subject = []
     input_file = open(filename)
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
+        # print(line)  # See what a line looks like
+        # print(repr(line))  # See what a line really looks like
         line = line.strip()  # Remove the \n
         parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
+        # print(parts)  # See what the parts look like (notice the integer is a string)
         # Make the number an integer as part of a new, poorly named, list
-        data = [parts[0], parts[1], int(parts[2])]
-        print(data)  # See if that worked
-        print("----------")
+        parts[2]= int(parts[2])
+        subject.append(parts)
+        print(subject)  # See if that worked
+        # print("----------")
     input_file.close()
+    return subject
+
+def display_subjects(subjects):
+    """Display subjects neatly"""
+    for subject in subjects:
+        print(f"{subject[0]} is taught by {subject[1]} and has {subject[2]} students")
+
 
 
 main()
