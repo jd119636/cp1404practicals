2793ea7e721345ada3f06ab11ec083adbb424d27
jd119636 <indra.kiiver@my.jcu.edu.au>
2026-08-11 15:14:57 +1000

    added username checkiing to list_exercises.py

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,14 +1,24 @@
-numbers = []
-first_number=input("Number:")
-second_number=input("Number:")
-third_number=input("Number:")
-fourth_number=input("Number:")
-fifth_number=input("Number:")
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 
-numbers.append(int(first_number))
-numbers.append(int(second_number))
-numbers.append(int(third_number))
-numbers.append(int(fourth_number))
-numbers.append(int(fifth_number))
+username = input("enter a username:")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
 
-print(f"The first number is {first_number}\nThe last number is {fifth_number}\nThe smallest number is {min(numbers)}\nThe largest number is {max(numbers)}\nThe average of the numbers is {sum(numbers)/ len(numbers)}")
+# numbers = []
+# first_number=input("Number:")
+# second_number=input("Number:")
+# third_number=input("Number:")
+# fourth_number=input("Number:")
+# fifth_number=input("Number:")
+#
+# numbers.append(int(first_number))
+# numbers.append(int(second_number))
+# numbers.append(int(third_number))
+# numbers.append(int(fourth_number))
+# numbers.append(int(fifth_number))
+#
+# print(f"The first number is {first_number}\nThe last number is {fifth_number}\nThe smallest number is {min(numbers)}\nThe largest number is {max(numbers)}\nThe average of the numbers is {sum(numbers)/ len(numbers)}")
+
