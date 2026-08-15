177b02744dcb73efe61c0dec9fa6443e70409b52
jd119636 <indra.kiiver@my.jcu.edu.au>
2026-08-15 21:59:47 +1000

    wimbledon done

---
+++ prac_05/wimbledon.py
@@ -0,0 +1,51 @@
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Wimbledon CSV file data loading, processing and displaying
+"""
+FILENAME = "wimbledon.csv"
+INDEX_COUNTRY = 1
+INDEX_CHAMPION = 2
+
+
+def main():
+    """Load data file and print details about Wimbledon champions and countries."""
+    records = load_records(FILENAME)
+    process_records(records)
+    champion_to_count, countries = process_records(records)
+    display_results(champion_to_count, countries)
+
+
+def load_records(filename):
+    """Load records from file in list of lists form."""
+    records = []
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
+        in_file.readline()  # Remove CSV header row
+        for line in in_file:
+            parts = line.strip().split(",")
+            records.append(parts)
+    return records
+
+
+def process_records(records):
+    """Create dictionary of champions and set of countries from records (list of lists)."""
+    champion_to_count = {}
+    countries = set()
+    for record in records:
+        countries.add(record[INDEX_COUNTRY])
+        try:
+            champion_to_count[record[INDEX_CHAMPION]] += 1
+        except KeyError:
+            champion_to_count[record[INDEX_CHAMPION]] = 1
+    return champion_to_count, countries
+
+def display_results(champion_to_count, countries):
+    """Display champions and countries."""
+    print("Wimbledon Champions: ")
+    for name, count in champion_to_count.items():
+        print(name, count)
+    print()
+    print(f"These {len(countries)} countries have won Wimbledon: ")
+    print(", ".join(sorted(countries)))
+
+
+main()
